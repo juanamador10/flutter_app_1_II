e8e861c4f2d215be7b32df8a0acf6272fb84370e
juanamador10 <juan.amador.is@unipolidgo.edu.mx>
2022-09-08 15:10:01 -0500

    Home Screen Design

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_1/helpers/database_helper.dart';
+import 'package:flutter_app_1/models/cat_model.dart';
 
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatefulWidget{
   const HomeScreen ({Key? key}): super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context){
-    return const MaterialApp();
+    
+    final textControllerRace = TextEditingController();
+    final textControllerName = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SQLite Example with Cats'), 
+        elevation: 0),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: [
+            TextFormField(
+              controller: textControllerRace, decoration: const InputDecoration(
+                icon: Icon(Icons.view_comfortable),
+                labelText: 'Input the race of cat'
+              ) 
+            ),
+            TextFormField(
+              controller: textControllerName, decoration: const InputDecoration(
+                icon: Icon(Icons.text_format_outlined),
+                labelText: 'Input the name of cat'
+              ) 
+            ),
+            Center(
+              child: (
+                FutureBuilder<List<Cat>> (
+                  future: DatabaseHelper.instance.getCats(),
+                  builder: (BuildContext context, AsyncSnapshot<List<Cat>> snapshot) {
+                    if(!snapshot.hasData){
+                      return Center(
+                        child: Container(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: const Text('Loading...'),
+                        ),
+                      );
+                    }
+                    else {
+                      return snapshot.data!.isEmpty ? Center(child: Container(child: const Text('No cats in the list'))) :
+                      ListView(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        children: snapshot.data!.map((cat) {
+                            return Center(
+                              child: ListTile(
+                                title: Text(cat.name),
+                              ),
+                            );
+                          }
+                        ).toList()
+                      );
+                    }
+                  },
+                )
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: 
+          Icon(Icons.save),
+          onPressed: () async {
+            DatabaseHelper.instance.add(
+              Cat(race: textControllerRace.text, name: textControllerName.text)
+            );
+            setState(() {
+                textControllerName.clear();
+                textControllerRace.clear();
+              }
+            );
+          },
+        ),
+    );
       }
-  }
+}
--- lib/helpers/database_helper.dart
+++ lib/helpers/database_helper.dart
@@ -3,6 +3,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../models/cat_model.dart';
+
 class DatabaseHelper {
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
@@ -15,4 +17,30 @@ class DatabaseHelper {
     String path = join (documentsDirectory.path, 'animals.db');
     return await openDatabase(path, version: 1, onCreate: _onCreate);
   }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute(
+      '''
+        CREATE TABLE cats (
+          id INTEGER PRIMARY KEY,
+          race TEXT,
+          name TEXT
+        )
+      '''
+    );
+  }
+
+  Future<List<Cat>> getCats() async (
+    Database db = await instance.database;
+    var cats = await db.query('cats', orderBy: 'race');
+
+    List<Cats> catsList = cats.isNotEmpty ? cats.map((e) => Cat.fromMap(e)).toList() : [];
+    return catsList
+  )
+
+  Future <int> add(Cat cat) async {
+    Database db = await instance.database;
+    return await db.insert('cats', cat.toMap());
+  }
+
 }
